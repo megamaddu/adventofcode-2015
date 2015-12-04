8260ce43b6e99fc36620511b9e7d2d05a9a1cd81
Michael Trotter <spicydonuts@gmail.com>
2015-12-04 14:04:25 -0700

    Day 1, part 2

--- 1/Main.hs
+++ 1/Main.hs
@@ -4,10 +4,23 @@ main :: IO ()
 main = do
   input <- getLine
   print $ calcFloor input
+  print $ calcStepsToBasementEntry input
 
 calcFloor :: String -> Integer
-calcFloor = foldl (\x c -> x + interp c) 0
+calcFloor = foldl interp 0
+
+calcStepsToBasementEntry :: String -> Integer
+calcStepsToBasementEntry = step 1 0
   where
-    interp '(' =  1
-    interp ')' = -1
-    interp _   =  0
+    step :: Integer -> Integer -> String -> Integer
+    step _ _ ""     = 0
+    step c t (x:xs) =
+      if t' < 0
+        then c
+        else step (c + 1) t' xs
+      where t' = interp t x
+
+interp :: Integer -> Char -> Integer
+interp x '(' = x + 1
+interp x ')' = x - 1
+interp x _   = x
